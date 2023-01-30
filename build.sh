0b7c043d1eea46284ce98375b1178e49ba63da38
Bjorn <bjorn.in.magnusson@gmail.com>
2023-01-30 18:40:19 +0100

    Add tip to fix if compiler complains in build file

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ fi
 set -e
 
 mkdir -p .build/$BUILD_TYPE
-cmake -B .build/$BUILD_TYPE -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+cmake -B .build/$BUILD_TYPE -DCMAKE_BUILD_TYPE=$BUILD_TYPE # try adding: -D CMAKE_C_COMPILER=gcc-compiler -D CMAKE_CXX_COMPILER=g++-compiler
 cmake --build .build/$BUILD_TYPE --parallel
