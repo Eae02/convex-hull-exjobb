b6950639ebda1a92a1b8b74fb323086d123c2454
Erik Amirell Eklöf <erik.amirell.eklof@gmail.com>
2023-02-14 15:13:37 +0100

    rotate in gensquare.cpp

--- testtools/generators/gensquare.cpp
+++ testtools/generators/gensquare.cpp
@@ -6,8 +6,15 @@ void generatePoints(std::vector<point>& points, rand_generator& rng) {
 	double min = getArgOrDefault("min", -100);
 	double max = getArgOrDefault("max", 100);
 	std::uniform_real_distribution<double> distribution(min, max);
+	
+	double rotation = std::uniform_real_distribution<double>(0, M_PI * 2)(rng);
+	double cosR = std::cos(rotation);
+	double sinR = std::sin(rotation);
+	
 	for (point& p : points) {
-		p.x = distribution(rng);
-		p.y = distribution(rng);
+		double x = distribution(rng);
+		double y = distribution(rng);
+		p.x = cosR * x - sinR * y;
+		p.y = sinR * y + cosR * x;
 	}
 }
