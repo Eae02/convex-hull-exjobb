e7bed556689a94b5c359cfde7166035513155128
Bjorn <bjorn.in.magnusson@gmail.com>
2023-03-03 15:36:43 +0100

    Fix bad numerics in quickhull causing it to generate wrong answers sometimes. This does however slow it down

--- src/implementations/quickhull.cpp
+++ src/implementations/quickhull.cpp
@@ -25,10 +25,10 @@ void quickhullRec(
 	point<T> normal = (rightHullPoint - leftHullPoint).rotated90CCW();
 	
 	size_t maxPointIdx = 0;
-	T maxPointDot = normal.dot(pts[0]);
+	T maxPointDot = normal.dot(pts[0] - leftHullPoint);
 	point<T> maxPoint = pts[0];
 	for (size_t i = 1; i < pts.size(); i++) {
-		T dotProduct = normal.dot(pts[i]);
+		T dotProduct = normal.dot(pts[i] - leftHullPoint);
 		if (std::tie(dotProduct, pts[i]) > std::tie(maxPointDot, maxPoint)) {
 			maxPointIdx = i;
 			maxPointDot = dotProduct;
